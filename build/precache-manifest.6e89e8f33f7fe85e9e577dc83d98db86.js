9b45a2800e9881e9b2756723f83f0973e53d0cd4
Mason Scott <scottm7575@gmail.com>
2020-04-08 15:42:56 -0500

    deployment

---
+++ build/precache-manifest.6e89e8f33f7fe85e9e577dc83d98db86.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d19bd995190c3d8b94a7b44d48054fc",
+    "url": "/Responsive-portfolio/index.html"
+  },
+  {
+    "revision": "06ee231a0b601cd00044",
+    "url": "/Responsive-portfolio/static/css/main.4d8b5d04.chunk.css"
+  },
+  {
+    "revision": "9dc16abdda55b3f4cbf0",
+    "url": "/Responsive-portfolio/static/js/2.b32cd597.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Responsive-portfolio/static/js/2.b32cd597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06ee231a0b601cd00044",
+    "url": "/Responsive-portfolio/static/js/main.09f1410c.chunk.js"
+  },
+  {
+    "revision": "0b456023b72af5baca0a",
+    "url": "/Responsive-portfolio/static/js/runtime-main.7bfbd9f9.js"
+  },
+  {
+    "revision": "f847455115ba9c5e7e22a059e2a4aebb",
+    "url": "/Responsive-portfolio/static/media/Mason.f8474551.PNG"
+  },
+  {
+    "revision": "6d339f8aee587b3a265eb5ecb7720775",
+    "url": "/Responsive-portfolio/static/media/d&d.6d339f8a.png"
+  },
+  {
+    "revision": "097b77494452f9352630bcf79697db4d",
+    "url": "/Responsive-portfolio/static/media/insta.097b7749.png"
+  },
+  {
+    "revision": "7fb99744eaccae943d5adf537b5b3a93",
+    "url": "/Responsive-portfolio/static/media/linkedin.7fb99744.png"
+  },
+  {
+    "revision": "05b91e012e87e0440fd8451a76b3357f",
+    "url": "/Responsive-portfolio/static/media/lofi.05b91e01.jpg"
+  },
+  {
+    "revision": "8eefb822f07c28f0598bea7d7b47927e",
+    "url": "/Responsive-portfolio/static/media/notebook.8eefb822.png"
+  },
+  {
+    "revision": "fc4fc38639750f3a53e265cab8f3b3ae",
+    "url": "/Responsive-portfolio/static/media/pokeball.fc4fc386.png"
+  }
+]);
